cb25df65586084c58d631e049bad7b264766fbb5
nebzter <68857144+nebzter@users.noreply.github.com>
2022-11-27 01:10:19 +0700

    Add files via upload

--- wp-admin/admin.php
+++ wp-admin/admin.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://nebzter.github.io/kreenthon_work_WP/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fpage%3Dsimply-static_settings&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://nebzter.github.io/kreenthon_work_WP">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://nebzter.github.io/kreenthon_work_WP/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fpage%3Dsimply-static_settings&reauth=1";
+			window.location = "https://nebzter.github.io/kreenthon_work_WP";
 		</script>
 
-		<p>You are being redirected to <a href="https://nebzter.github.io/kreenthon_work_WP/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fpage%3Dsimply-static_settings&reauth=1">https://nebzter.github.io/kreenthon_work_WP/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fadmin.php%3Fpage%3Dsimply-static_settings&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://nebzter.github.io/kreenthon_work_WP">https://nebzter.github.io/kreenthon_work_WP</a></p>
 	</body>
 </html>
